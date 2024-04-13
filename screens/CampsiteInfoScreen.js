093bb6dccc36e5de78645841565e0a8e4b63b2b6
Jose V <alvajose35@gmail.com>
2024-04-13 00:47:21 -0400

    Navigation Part 2

--- screens/CampsiteInfoScreen.js
+++ screens/CampsiteInfoScreen.js
@@ -1,6 +1,6 @@
 import RenderCampsite from "../features/campsites/RenderCampsite";
 
-const CampsiteInfoScreen = ({ route}) => {
+const CampsiteInfoScreen = ({ route }) => {
 	const { campsite } = route.params;
 	return <RenderCampsite campsite={campsite} />;
 };
