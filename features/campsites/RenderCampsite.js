c4f824342525c6693384d3731d5c4f813e47bdd5
Jose V <alvajose35@gmail.com>
2024-04-28 01:49:07 -0400

    Gestures part 1

--- features/campsites/RenderCampsite.js
+++ features/campsites/RenderCampsite.js
@@ -1,19 +1,47 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, PanResponder, Alert } from "react-native";
 import { Card, Icon } from "react-native-elements";
 import { baseUrl } from "../../shared/baseUrl";
 import * as Animatable from 'react-native-animatable';
 
 const RenderCampsite = (props) => {
-	
+
 	// Destructure campsite prop from CampsiteInfoScreen
 	const { campsite } = props;
 
-	if(campsite) {
+	const isLeftSwipe = ({ dx }) => dx < -200;
+
+	const panResponder = PanResponder.create({
+		onStartShouldSetPanResponder: () => true,
+		onPanResponderEnd: (e, gestureState) => {
+			console.log('pan responder end', gestureState);
+			if (isLeftSwipe(gestureState)) {
+				Alert.alert(
+					'Add Favorite',
+					'Are you sure you wish to add ' + campsite.name + ' to favorites?',
+					[
+						{
+							text: 'Cancel',
+							style: 'cancel',
+							onPress: () => console.log('Cancel Pressed')
+						},
+						{
+							text: 'OK',
+							onPress: () => props.isFavorite ? console.log('Already set as a favorite')	: props.markFavorite()
+						}
+					],
+					{ cancelable: false }
+				);
+			}
+		}
+	});
+
+	if (campsite) {
 		return (
 			<Animatable.View
 				animation='fadeInDownBig'
 				duration={2000}
 				delay={1000}
+				{...panResponder.panHandlers}
 			>
 				<Card containerStyle={styles.cardContainer}>
 					<Card.Image source={{ uri: baseUrl + campsite.image }}>
@@ -31,10 +59,10 @@ const RenderCampsite = (props) => {
 							color='#f50'
 							raised
 							reverse
-							onPress={() => 
+							onPress={() =>
 								props.isFavorite
-								? console.log('Already set as a favorite')
-								:  props.markFavorite()
+									? console.log('Already set as a favorite')
+									: props.markFavorite()
 							}
 						/>
 						<Icon
@@ -50,7 +78,7 @@ const RenderCampsite = (props) => {
 			</Animatable.View>
 		);
 	}
-	return 	<View />
+	return <View />
 };
 
 const styles = StyleSheet.create({
