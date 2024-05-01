67f527f701cea29fbe2958c00e47329ca6abc3f0
Jose V <alvajose35@gmail.com>
2024-04-30 21:51:16 -0400

    Secure Store

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -9,6 +9,7 @@ import AboutScreen from "./AboutScreen";
 import ContactScreen from "./ContactScreen";
 import ReservationScreen from "./ReservationScreen";
 import FavoritesScreen from './FavoritesScreen';
+import LoginScreen from "./LoginScreen";	
 import { Icon } from "react-native-elements";
 import logo from "../assets/images/logo.png";
 import { useDispatch } from "react-redux";
@@ -83,6 +84,28 @@ const FavoritesNavigator = () => {
 					headerLeft: () => (
 						<Icon
 							name="heart"
+							type="font-awesome"
+							iconStyle={styles.stackIcon}
+							onPress={() => navigation.toggleDrawer()}
+						/>
+					)
+				})}
+			/>
+		</Stack.Navigator>
+	);
+};
+
+const LoginNavigator = () => {
+	const Stack = createStackNavigator();
+	return (
+		<Stack.Navigator screenOptions={screenOptions} >
+			<Stack.Screen
+				name='Login'
+				component={LoginScreen}
+				options={({ navigation }) => ({
+					headerLeft: () => (
+						<Icon
+							name="sign-in"
 							type="font-awesome"
 							iconStyle={styles.stackIcon}
 							onPress={() => navigation.toggleDrawer()}
@@ -209,6 +232,21 @@ const Main = () => {
 				drawerContent={CustomDrawerContent}
 				drawerStyle={{ backgroundColor: '#CEC8FF' }} // This feature is not working on our apps
 			>
+				<Drawer.Screen
+					name='Login'
+					component={LoginNavigator}
+					options={{
+						drawerIcon: ({ color }) => (
+							<Icon
+								name="sign-in"
+								type="font-awesome"
+								size={24}
+								iconStyle={{ width: 20 }}
+								color={color}
+							/>
+						)
+					}}
+				/>
 				<Drawer.Screen
 					name='Home'
 					component={HomeNavigator}
